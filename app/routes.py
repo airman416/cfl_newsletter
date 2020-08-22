61ede141fe13665fd6eb780b4b65dfff3e4644ca
“airman416” <im.armaan.agrawal@gmail.com>
2020-08-22 23:44:58 +0900

    Templating; Sample posts; --Next add pagination and database for posts.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,32 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-
 @app.route('/index')
 def index():
-    return "Hello, world"
+    contacts = [
+        {
+            'name': 'GitHub',
+            'url': 'https://github.com/KIST-CFL'
+        }
+    ]
+    
+    posts = [
+        {
+            'author': {'username': 'airman416'},
+            'date': '2020',
+            'title': 'First Post',
+            'body': 'First post on the new CFL Newsletter website!',
+            'topic': 'Python'
+        }
+    ]
+    
+    topics = []
+    for post in posts:
+        print(post)
+        if post['topic'] in topics:
+            continue
+        else:
+            topics.append(post['topic'])
+        
+    return render_template('index.html', title='Home', posts=posts, contacts=contacts, topics=topics)
